970be8c39d90f48f2fb202af47bb97f0fcb0f33c
suho0515 <suho0515_@naver.com>
2020-10-20 02:23:29 -0400

    Add files via upload

--- src/mavros_teleop_key.cpp
+++ src/mavros_teleop_key.cpp
@@ -10,6 +10,8 @@
 #include "geometry_msgs/PoseStamped.h"
 #include "geometry_msgs/Vector3Stamped.h"
 #include "geometry_msgs/Twist.h"
+#include "geometry_msgs/TwistStamped.h"
+#include "mavros_msgs/AttitudeTarget.h"
 #include "linux_kbhit.h"
 
 int count = 1;
@@ -50,39 +52,92 @@ double p_x = 0.0;
 double p_y = 0.0;
 double p_z = 2.0;
 
-void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& pose_msg,ros::Publisher _pub, ros::Publisher _vel_pub) {
+double br_x = 0.0;
+double br_y = 0.0;
+double br_z = 0.0;
+double thrust = 0.5;
+
+double l_x = 0.0;
+double l_y = 0.0;
+double l_z = 0.0;
+
+double a_x = 0.0;
+double a_y = 0.0;
+double a_z = 0.0;
+
+void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& pose_msg, ros::Publisher att_pub, ros::Publisher vel_pub) {
    float x = pose_msg->pose.position.x; 
    float y = pose_msg->pose.position.y; 
    float z = pose_msg->pose.position.z; 
-   //ROS_INFO("x = %f", x);
-   //ROS_INFO("y = %f", y);
-   //ROS_INFO("z = %f", z);
+   ROS_INFO("x = %f", x);
+   ROS_INFO("y = %f", y);
+   ROS_INFO("z = %f", z);
 
-   geometry_msgs::Twist vel_msg;
-
+   geometry_msgs::TwistStamped vel_msg;
 
    geometry_msgs::PoseStamped msg;
 
+   mavros_msgs::AttitudeTarget att_msg;
    
+   int ch;
+   if(ch = kbhit()) {
+      ROS_INFO("You pressed '%c'!", ch);
+      if(ch == 's') {
+         br_x = 0.0;
+         br_y = 0.0;
+         br_z = 0.0;
+         thrust = 0.5;
+
+         l_x = 0.0;
+         l_y = 0.0;
+         l_z = 0.0;
+
+         a_x = 0.0;
+         a_x = 0.0;
+         a_x = 0.0;
+      }
+      else if(ch == 't') {
+         br_x = 0.0;
+         br_y = 0.0;
+         br_z = 0.0;
+         thrust = 5.0;
+
+         l_x = 0.0;
+         l_y = 0.0;
+         l_z = 1.0;
+
+         a_x = 0.0;
+         a_x = 0.0;
+         a_x = 0.0;
+      }
+      else if(ch == 'x') p_y += 0.1;
+      else if(ch == 'w') p_y -= 0.1;
+   }
 
    if(!proc) {
-      msg.header.stamp = ros::Time::now();
-      msg.header.seq=count;
-      msg.header.frame_id = 1;
-      msg.pose.position.x = p_x;//0.001*some_object.position_x;
-      msg.pose.position.y = p_y;//0.001*some_object.position_y;
-      msg.pose.position.z = p_z;//0.001*some_object.position_z;
-      msg.pose.orientation.x = 0;
-      msg.pose.orientation.y = 0;
-      msg.pose.orientation.z = 0;
-      msg.pose.orientation.w = 1;
+      
+      att_msg.orientation.x = 0;
+      att_msg.orientation.y = 0;
+      att_msg.orientation.z = 0;
+      att_msg.orientation.w = 1;
 
-      _pub.publish(msg);
+      att_msg.body_rate.x = br_x;
+      att_msg.body_rate.y = br_y;
+      att_msg.body_rate.z = br_z;
+
+      att_msg.thrust = thrust;
+
+
+
+      att_pub.publish(att_msg);
+
       ros::spinOnce();
         
+      ROS_INFO("trying to take off");
 
       if(x > -0.5 && x < 0.5 && y > -0.5 && y < 0.5 && z > 1.5 && z < 2.5) proc++;
    }
+   /*
    else if(proc == 1) {
       int ch;
       if(x > x-0.1 && x < x+0.1 && y > y-0.1 && y < y+0.1 && z > z-0.1 && z < z+0.1) {
@@ -95,6 +150,7 @@ void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& pose_msg,ros::Publ
             
          }
       }
+   
 
       
       
@@ -112,77 +168,7 @@ void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& pose_msg,ros::Publ
       _pub.publish(msg);
       ros::spinOnce();
    }
-   // else if(proc == 1) {
-   //    msg.header.stamp = ros::Time::now();
-   //    msg.header.seq=count;
-   //    msg.header.frame_id = 1;
-   //    msg.pose.position.x = 5.0;//0.001*some_object.position_x;
-   //    msg.pose.position.y = 0.0;//0.001*some_object.position_y;
-   //    msg.pose.position.z = 2.0;//0.001*some_object.position_z;
-   //    msg.pose.orientation.x = 0;
-   //    msg.pose.orientation.y = 0;
-   //    msg.pose.orientation.z = 0;
-   //    msg.pose.orientation.w = 1;
-
-   //    if(x > 4.5 && x < 5.5 && y > -0.5 && y < 0.5 && z > 1.5 && z < 2.5) proc++;
-   // }
-   // else if(proc == 2) {
-   //    msg.header.stamp = ros::Time::now();
-   //    msg.header.seq=count;
-   //    msg.header.frame_id = 1;
-   //    msg.pose.position.x = 5.0;//0.001*some_object.position_x;
-   //    msg.pose.position.y = 10.0;//0.001*some_object.position_y;
-   //    msg.pose.position.z = 2.0;//0.001*some_object.position_z;
-   //    msg.pose.orientation.x = 0;
-   //    msg.pose.orientation.y = 0;
-   //    msg.pose.orientation.z = 0;
-   //    msg.pose.orientation.w = 1;
-
-   //    if(x > 4.5 && x < 5.5 && y > 9.5 && y < 10.5 && z > 1.5 && z < 2.5) proc++;
-   // }
-   // else if(proc == 3) {
-   //    msg.header.stamp = ros::Time::now();
-   //    msg.header.seq=count;
-   //    msg.header.frame_id = 1;
-   //    msg.pose.position.x = -5.0;//0.001*some_object.position_x;
-   //    msg.pose.position.y = 10.0;//0.001*some_object.position_y;
-   //    msg.pose.position.z = 2.0;//0.001*some_object.position_z;
-   //    msg.pose.orientation.x = 0;
-   //    msg.pose.orientation.y = 0;
-   //    msg.pose.orientation.z = 0;
-   //    msg.pose.orientation.w = 1;
-
-   //    if(x < -4.5 && x > -5.5 && y > 9.5 && y < 10.5 && z > 1.5 && z < 2.5) proc++;
-   // }   
-   // else if(proc == 4) {
-   //    msg.header.stamp = ros::Time::now();
-   //    msg.header.seq=count;
-   //    msg.header.frame_id = 1;
-   //    msg.pose.position.x = -5.0;//0.001*some_object.position_x;
-   //    msg.pose.position.y = 0.0;//0.001*some_object.position_y;
-   //    msg.pose.position.z = 2.0;//0.001*some_object.position_z;
-   //    msg.pose.orientation.x = 0;
-   //    msg.pose.orientation.y = 0;
-   //    msg.pose.orientation.z = 0;
-   //    msg.pose.orientation.w = 1;
-
-   //    if(x < -4.5 && x > -5.5 && y > -0.5 && y < 0.5 && z > 1.5 && z < 2.5) proc++;
-   // }   
-   // else if(proc == 5) {
-   //    msg.header.stamp = ros::Time::now();
-   //    msg.header.seq=count;
-   //    msg.header.frame_id = 1;
-   //    msg.pose.position.x = 0.0;//0.001*some_object.position_x;
-   //    msg.pose.position.y = 0.0;//0.001*some_object.position_y;
-   //    msg.pose.position.z = 2.0;//0.001*some_object.position_z;
-   //    msg.pose.orientation.x = 0;
-   //    msg.pose.orientation.y = 0;
-   //    msg.pose.orientation.z = 0;
-   //    msg.pose.orientation.w = 1;
-
-   //    if(x > -0.5 && x < 0.5 && y > -0.5 && y < 0.5 && z > 1.5 && z < 2.5) proc=0;
-   // }   
-
+   */
 
    count++; 
 
@@ -196,11 +182,13 @@ int main(int argc, char **argv)
    ros::NodeHandle n;
 
    // ros publisher
-   ros::Publisher chatter_pub = n.advertise<geometry_msgs::PoseStamped>("/mavros/setpoint_position/local",100);
-   ros::Publisher vel_pub = n.advertise<geometry_msgs::Twist>("/mavros/setpoint_velocity/cmd_vel",100);
+   //ros::Publisher chatter_pub = n.advertise<geometry_msgs::PoseStamped>("/mavros/setpoint_position/local",100);
+   ros::Publisher vel_pub = n.advertise<geometry_msgs::TwistStamped>("/mavros/setpoint_attitude/cmd_vel",10);
+   ros::Publisher att_pub = n.advertise<mavros_msgs::AttitudeTarget>("mavros/setpoint_raw/attitude", 10); 
+
 
    // ros subscriber
-   ros::Subscriber pose_sub = n.subscribe<geometry_msgs::PoseStamped>("/mavros/local_position/pose", 100, boost::bind(poseCallback, _1, chatter_pub, vel_pub));
+   ros::Subscriber pose_sub = n.subscribe<geometry_msgs::PoseStamped>("/robot_pose", 100, boost::bind(poseCallback, _1, att_pub, vel_pub));
 
    ros::spin();
       
